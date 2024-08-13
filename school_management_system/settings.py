bd79df19d109961d740df2e5cf6a752c7bf9d4e1
SteveNdegwa <stevencallistus19@gmail.com>
2024-08-13 05:29:33 +0300

    feat: connect to postgres

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'school_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'schoolmanagement',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'schoolmanagement.cfysi4uy2jv2.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
