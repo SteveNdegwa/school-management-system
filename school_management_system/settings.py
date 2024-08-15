3b037f14975146747abfe62d5879c7631e56703f
SteveNdegwa <stevencallistus19@gmail.com>
2024-08-15 05:02:11 +0300

    updates

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -134,3 +134,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_ACCESS_KEY_ID = 'AKIAYZZGSZYWQQQP2DX3'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 's3-school-management'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
